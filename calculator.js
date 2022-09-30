6447b8fe95a682c5e59eae75d9122dfd2fc55364
letherius Miller <letherius1@gmail.com>
2022-09-30 16:17:44 -0500

    9/30/22

--- calculator.js
+++ calculator.js
@@ -1,13 +1,12 @@
 function Calculator(){
-  console.log("Calculating");
+  console.log("Calculator");
   let num1 = Number(prompt("Enter num1:"));
   let num2 = Number(prompt("Enter num2:"));
   sum(num1,num2);
-
 }
+
 
 function sum(a,b){
   let total = a + b;
 document.getElementById("sumRes").innerHTML=`<p> ${a} + ${b} = ${total} </p>`;
-
-  }
+}
