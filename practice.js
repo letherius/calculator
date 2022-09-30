d2aa56d07a2a846110e1bab0034274dd807f80fc
letherius <112995678+letherius@users.noreply.github.com>
2022-09-29 22:01:10 -0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,13 @@
+function hello(name){
+    console.log("Hello "+name);
+}
+
+//challenge time
+
+//Define a function called multilyThree which accepts a number and returns that number multiplied by 3.
+
+function multiplythree(number){
+    let total = number * 5;
+    console.log("Result: "+total);
+}
+multiplythree(3);
